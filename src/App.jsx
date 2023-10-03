c7dec79da6d77b8f64f4ceef3d591cdf6d2a759e
Bomowo <oeccc25@yahoo.com>
2023-10-03 19:26:22 +0300

    Fix test case 2 (?)

--- src/App.jsx
+++ src/App.jsx
@@ -19,21 +19,27 @@ function App() {
 
   return (
     <div id="drum-machine" className="drum-machine" onKeyDown={pressKeyboard} tabIndex='0'>
-      <div className="drum-pad" onClick={clickOnPad}>
-        <audio className="clip" id="Q" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"></audio>
-        Q</div>
+        <div id="display"></div>
         <div className="drum-pad" onClick={clickOnPad}>
-        <audio className="clip" id="W" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"></audio>
-        W</div>
+          <audio className="clip" id="Q" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"></audio>
+          Q
+        </div>
         <div className="drum-pad" onClick={clickOnPad}>
-        <audio className="clip" id="E" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"></audio>
-        E</div>
+          <audio className="clip" id="W" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"></audio>
+          W
+        </div>
         <div className="drum-pad" onClick={clickOnPad}>
-        <audio className="clip" id="A" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"></audio>
-        A</div>
+          <audio className="clip" id="E" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"></audio>
+          E
+        </div>
+        <div className="drum-pad" onClick={clickOnPad}>
+          <audio className="clip" id="A" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"></audio>
+          A
+        </div>
         <div className="drum-pad" onClick={clickOnPad}>
         <audio className="clip" id="S" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"></audio>
-        S</div>
+        S
+        </div>
         <div className="drum-pad" onClick={clickOnPad}>
         <audio className="clip" id="D" src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"></audio>
         D</div>
@@ -46,7 +52,6 @@ function App() {
         <div className="drum-pad" onClick={clickOnPad}>
         <audio className="clip" id="C" src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"></audio>
         C</div>
-
     </div>
   )
 }
